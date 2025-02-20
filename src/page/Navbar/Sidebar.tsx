27dce661f8d927750a17ecf57898f26672602f01
trungnq555 <trungnq555gmail.com>
2025-02-20 09:49:40 +0700

    feat(system): create base project

--- src/page/Navbar/Sidebar.tsx
+++ src/page/Navbar/Sidebar.tsx
@@ -1,27 +1,48 @@
 import React from 'react'
-import {HomeIcon} from "lucide-react";
+
 import {Button} from "../../components/ui/button"
+import {ActivityLogIcon, DashboardIcon, ExitIcon, HomeIcon, PersonIcon} from "@radix-ui/react-icons";
+import {BookmarkIcon, CreditCardIcon, LandmarkIcon, WalletIcon} from "lucide-react";
+import {useNavigation} from "react-router-dom";
+import {
+    SheetClose,
+} from "../../components/ui/sheet"
+
 
 const menu = [
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
-  {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>}
+    {name: "Home", path: "/", icon: <HomeIcon className="h-6 w-6"/>},
+    {name: "Portfolio", path: "/portfolio", icon: <DashboardIcon className="h-6 w-6"/>},
+    {name: "Watchlist", path: "/watchlist", icon: <BookmarkIcon className="h-6 w-6"/>},
+    {name: "Activity", path: "/activity", icon: <ActivityLogIcon className="h-6 w-6"/>},
+    {name: "Wallet", path: "/wallet", icon: <WalletIcon className="h-6 w-6"/>},
+    {name: "Payment Details", path: "/payment-details", icon: <LandmarkIcon className="h-6 w-6"/>},
+    {name: "Withdrawal", path: "/withdrawal", icon: <CreditCardIcon className="h-6 w-6"/>},
+    {name: "Profile", path: "/profile", icon: <PersonIcon className="h-6 w-6"/>},
+    {name: "Logout", path: "/profile", icon: <ExitIcon className="h-6 w-6"/>},
+
 ]
 
 const Sidebar = () => {
-  return (
-    <div className="mt-10 space-y-5 hover:bg-gray-800">
-      <div>
-        <Button className="flex items-center gap-5 py-6 w-full">
-          <span className="w-8"><HomeIcon/></span>
-          <p>Home</p>
-        </Button>
-      </div>
-    </div>
-  )
+    // const navigation = useNavigation();
+    // const dispatch = useDispatch();
+    // const handleLogout = () => {
+    //     dispatch(logout());
+    // };
+
+    return (
+        <div className="mt-10 space-y-5">
+            {menu.map((item) => (
+                <div key={item.name}>
+                    <SheetClose className="w-full ">
+                        <Button variant="outline" className="flex items-center gap-5 py-6 w-full hover:bg-gray-400">
+                            <span className="w-8">{item.icon}</span>
+                            <p>{item.name}</p>
+                        </Button>
+                    </SheetClose>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Sidebar
