321ed9b187a864aa6714ca5d55051a22b6a9d018
trungnq555 <trungnq555gmail.com>
2025-02-20 15:59:10 +0700

    feat(project): create currency chart

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -1,6 +1,9 @@
 import {Button} from "@/components/ui/button"
 import React from "react";
-import AssetTable from "../AssetTable";
+import AssetTable from "./AssetTable";
+import StockChart from "./StockChart";
+import {Avatar, AvatarImage} from "@radix-ui/react-avatar";
+import {DotIcon, MessageCircle} from "lucide-react";
 
 const Home = () => {
     const [category, setCategory] = React.useState("all");
@@ -13,17 +16,63 @@ const Home = () => {
         <div className="relative">
             <div className="lg:flex ">
                 <div className="lg:w-1/2 lg:border-b">
-                    <div className="p-3 flex items-center gap-4  *:hover:bg-white *:hover:text-black *:selection:bg-white">
-                       <Button onClick={() => handleCategory("all")} className="rounded-full" variant={category=="all" ? "default" : "outline"}>All</Button>
-                       <Button onClick={() => handleCategory("top50")} className="rounded-full" variant={category=="top50" ? "default" : "outline"}>Top 50</Button>
-                       <Button onClick={() => handleCategory("topGainers")} className="rounded-full" variant={category=="topGainers" ? "default" : "outline"}>Top Gainers</Button>
-                       <Button onClick={() => handleCategory("topLosers")} className="rounded-full" variant={category=="topLosers" ? "default" : "outline"}>Top Losers</Button>
+                    <div
+                        className="p-3 flex items-center gap-4  *:hover:bg-white *:hover:text-black *:selection:bg-white">
+                        <Button onClick={() => handleCategory("all")} className="rounded-full"
+                                variant={category == "all" ? "default" : "outline"}>All</Button>
+                        <Button onClick={() => handleCategory("top50")} className="rounded-full"
+                                variant={category == "top50" ? "default" : "outline"}>Top 50</Button>
+                        <Button onClick={() => handleCategory("topGainers")} className="rounded-full"
+                                variant={category == "topGainers" ? "default" : "outline"}>Top Gainers</Button>
+                        <Button onClick={() => handleCategory("topLosers")} className="rounded-full"
+                                variant={category == "topLosers" ? "default" : "outline"}>Top Losers</Button>
                     </div>
                     <AssetTable/>
                 </div>
+                <div className="hidden lg:block lg:w-1/2">
+                    <StockChart/>
+                    <div className="flex gap-3 items-center py-7 pl-4">
+                        <div>
+                            <Avatar>
+                                <AvatarImage
+                                    src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png"}
+                                    width={50} height={50}/>
+                            </Avatar>
+                        </div>
+                        <div>
+                            <div className=" items-center gap-2">
+                                <p>DOGE</p>
+                                <p className="text-xl font-bold">1622</p>
+
+                            </div>
+                        </div>
+                        <DotIcon className="text-gray-400"/>
+                        <div className=" items-end gap-2">
+
+                            <p className="text-gray-400">Doge Coin</p>
+                            <p>
+                                <span className="text-red-600">-123124312412413123</span>
+                                <span>(-0.2526982%)</span>
+                            </p>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
 
-    </div>
+            <section
+                className="absolute bottom-5 right-5 -translate-y-3 z-40 flex flex-col justify-end items-end gap-2">
+                <div className="relative w-[10rem] group">
+                    <Button className="w-full h-[3rem] cursor-pointer gap-2 items-center bg-white text-black">
+                        <MessageCircle className="fill-[#1e293b]  -rotate-90 stroke-none group-hover:fill-[#1a1a1a]"/>
+                        <span className="text-2xl">Chat Bot</span>
+                    </Button>
+                </div>
+
+            </section>
+
+        </div>
     )
 }
 
---
+++ src/page/Home/StockChart.tsx
@@ -0,0 +1,122 @@
+import Chart from 'react-apexcharts'
+import React, {useState} from "react";
+import ReactApexChart from 'react-apexcharts';
+import { Button } from '@/components/ui/button';
+
+const timeSeries = [{
+    keyword: "DIGITAL_CURRENCY_DAILY",
+    key: "Time Series (Daily)",
+    label: "1 Day",
+    value: 1,
+},{
+    keyword: "DIGITAL_CURRENCY_WEEKLY",
+    key: "Weekly Time Series",
+    label: "1 Week",
+    value: 7,
+},{
+    keyword: "DIGITAL_CURRENCY_MONTHLY",
+    key: "Month Time Series",
+    label: "1 Month",
+    value: 30,
+},{
+    keyword: "DIGITAL_CURRENCY_YEARLY",
+    key: "Year Time Series",
+    label: "365 Day",
+    value: 365,
+}]
+
+const StockChart = () => {
+
+    const [activeLabel, setActiveLabel] = useState("1 Day");
+
+    const series = [{
+        data:[ [1737446549875, 101885.476079607],
+            [1737450224058, 102336.848626247],
+        [1737453724125, 102574.846291047],
+        [1737457405375, 103051.920199296],
+        [1737460870436, 103680.996945755],
+        [1737464631018, 104383.967043879],
+        [1737468504633, 104476.607037582],
+        [1737471824899, 103459.591110279],
+        [1737475589188, 104071.020141236],
+        [1737479016864, 104858.631808125],
+        [1737482626637, 106132.122146877],
+        [1737486219258, 106789.70708043],
+        [1737489830545, 106969.681008106],
+        [1737493418206, 106189.882951434],
+        [1737497033206, 106817.77660527],
+        [1737500628136, 106011.735519903],
+        [1737504236285, 106193.341794117],
+        [1737508071434, 106194.217801489],
+        [1737511426442, 105769.314576742],
+        [1737515022084, 105588.500790401],
+        [1737518512618, 105676.939311479],
+        [1737522358507, 105715.690679462]]
+    }];
+
+    const options = {
+        chart: {
+            id: "area-datetime",
+            type: "area",
+            zoom: {
+                autoScaleYaxis: true
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        xaxis: {
+            type: 'datetime',
+            tickAmount: 6
+        },
+        colors: ["#758AA2"],
+        markers: {
+            color: ["#ffffff"],
+            strokeColor: '#fff',
+            size: 0,
+            strokeWidth: 1,
+            style: 'hollow'
+        },
+        tooltip: {
+            theme: 'dark'
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shadeIntensity: 1,
+                opacityForm: 0.8,
+                opacity: 0.9,
+                stops: [0,100]
+            }
+        },
+        grid: {
+            borderColor: "#47535E",
+            strokeDasharray: 4,
+            show: true
+        }
+    }
+
+    const handleActiveLabel = (value) => {
+        setActiveLabel(value);
+    }
+
+    return (
+        <div>
+            <div className="space-x-4 p-2 mt-1 ">
+                {timeSeries.map(((item) =>
+                    <Button key={item.label}
+                            onClick={() => handleActiveLabel(item.label)}
+                            className={`text-white border cursor-pointer hover:bg-white hover:text-black ${activeLabel == item.label ? "text-red" : "text-white"}`}>
+                        {item.label}
+                    </Button>))}
+
+            </div>
+            <div id="chart-timelines">
+                <ReactApexChart options={options} series={series} type='area' height={450}/>
+            </div>
+        </div>
+
+    )
+}
+
+export default StockChart
